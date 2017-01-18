71f48beff5ba0ae3da1dfef7534991ef8a6a73d6
alois <alois>
2017-01-18 02:13:42 -0500

    missing import

--- dist/util/logger.js
+++ dist/util/logger.js
@@ -12,9 +12,13 @@ var _uuid = require('uuid');
 
 var _uuid2 = _interopRequireDefault(_uuid);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var logPath = process.env.LOG_PATH ? path.join(process.env.LOG_PATH, _uuid2.default.v4() + '.log') : './app.log';
+var logPath = process.env.LOG_PATH ? _path2.default.join(process.env.LOG_PATH, _uuid2.default.v4() + '.log') : './app.log';
 
 function getDefaultStreams() {
   var streams = [{
